e20da7e9b4f074657eaf3a540684f204f5a0af42
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2021-12-18 21:38:43 -0600

    Add files via upload

---
+++ Dec15.js
@@ -0,0 +1,19 @@
+const age = 15;
+const isOldEnough = age >= 18;
+
+if (age >=) {
+    console.log('Jazmine can start driving license 🚗 ')
+} else {
+    const YearsLeft = 18 - age;
+    console.log('Jazmine is to young. Wait another ${yearsLeft} years :)');
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthyear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
